22f1bfe5f71e17964e92a92002fa1de0abeaad65
tribunskiy <forty-7@yandex.ru>
2017-07-10 13:00:26 +0300

    update

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ let expring = require('expring')
 
 let app = expring()
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
 let TaskController = require('./controllers/TaskController.js')
 TaskController.init(app)
 
